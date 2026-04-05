c6d73052305d492bd8883183984a83f21f49a25c
ggihan <140435642+GGihan@users.noreply.github.com>
2026-04-05 11:36:43 +0200

    Add footer with links to social media

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import github from "../assets/github.svg"
+import linkedin from "../assets/linkedin.svg"
+import twitter from "../assets/twitter.svg"
+
+export default function Footer() {
+
+  return (
+     <footer>
+      <nav className="socials" aria-label="Social media links">
+        <ul>
+          <li>
+            <a 
+              href="https://github.com/GGihan"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub profile (opens in a new tab)"
+            >
+              <img src={github} alt="" role="presentation" width={40} />
+            </a>
+          </li>
+          <li>
+            <a 
+              href="https://www.linkedin.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="linkedin homepage (opens in a new tab)"
+            >
+              <img src={linkedin} alt="" role="presentation" width={40} />
+            </a>
+          </li>
+          <li>
+            <a 
+              href="https://x.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter homepage (opens in a new tab)"
+            >
+              <img src={twitter} alt="" role="presentation" width={40} />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <p>@GGihan</p>
+    </footer>
+  );
+}
